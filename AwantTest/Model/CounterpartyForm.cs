d02a8b131c351004c973b20f53a69e37701b0d14
BAKAWAKALAKA <katryn36@gmail.com>
2019-12-24 15:11:39 +0300

    fix added forgoten requered attribute

--- AwantTest/Model/CounterpartyForm.cs
+++ AwantTest/Model/CounterpartyForm.cs
@@ -1,14 +1,16 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace AwantTest.Model
 {
     public class CounterpartyForm
     {
         [JsonProperty("inn")]
+        [Required]
         public long INN { get; set; }
 
         [JsonProperty("kpp")]
